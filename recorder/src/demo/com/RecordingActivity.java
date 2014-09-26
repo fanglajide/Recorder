8ee3dd92c88af55d7924a3521d325123e2fbbc11
chanlevel <fanglajide@126.com>
2014-09-26 22:04:08 +0800

    init

---
+++ recorder/src/demo/com/RecordingActivity.java
@@ -0,0 +1,43 @@
+package demo.com;
+
+import java.io.File;
+import java.io.IOException;
+
+import demo.com.RecordButton.OnFinishedRecordListener;
+import demo.com.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+
+public class RecordingActivity extends Activity {
+
+	private RecordButton mRecordButton = null;
+
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+		mRecordButton = (RecordButton) findViewById(R.id.record_button);
+
+		String path = Environment.getExternalStorageDirectory()
+				.getAbsolutePath()+"/com.dci.android.ssp";
+		
+		File file = new File(path);
+		if (!file.exists())
+			file.mkdirs();
+		path += "/mmmm.amr";
+		mRecordButton.setSavePath(path);
+		mRecordButton
+				.setOnFinishedRecordListener(new OnFinishedRecordListener() {
+
+					@Override
+					public void onFinishedRecord(String audioPath) {
+						Log.i("RECORD!!!", "finished!!!!!!!!!! save to "
+								+ audioPath);
+
+					}
+				});
+
+	}
+}
